3a5e3c912f3d9b2beb1c0fadca634ae1289f4119
Huy <tuanhuyanime@gmail.com>
2024-06-12 17:04:08 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+// CLI: npm install express body-parser --save
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+// APIsf
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.use('/api/admin', require('./api/admin.js'));
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,13 @@
+const MyConstants = {
+    DB_SERVER: 'shopping.bh1xahv.mongodb.net',
+    DB_USER: 'tuanhuyanime',
+    DB_PASS: '79a08148',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: '<email_user>',
+    EMAIL_PASS: '<email_pass>',
+    JWT_SECRET: 'KHOA_BI_MAT',
+    JWT_EXPIRES: '220120032174802010745', // in milliseconds
+  };
+  
+  module.exports = MyConstants;
+  
