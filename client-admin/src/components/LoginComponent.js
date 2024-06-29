0db5c6ca523196214ac1a2d6e0514aed0d050422
longstory <nguyenphamquynhlong123@gmail.com>
2024-06-29 13:59:13 +0700

    cap nhat 29/06

--- client-admin/src/components/LoginComponent.js
+++ client-admin/src/components/LoginComponent.js
@@ -1,9 +1,13 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import axios from 'axios';
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+import '../assets/Login.scss';
 import MyContext from '../contexts/MyContext';
 
 class Login extends Component {
   static contextType = MyContext; // using this.context to access global state
+
   constructor(props) {
     super(props);
     this.state = {
@@ -11,34 +15,86 @@ class Login extends Component {
       txtPassword: ''
     };
   }
+  handleShowHidePassword = () => {
+    this.setState({
+      isshowPassword: !this.state.isshowPassword
+    })
+
+  }
+
   render() {
     if (this.context.token === '') {
       return (
-        <div className="align-valign-center">
-          <h2 className="text-center">ADMIN LOGIN</h2>
-          <form>
-            <table className="align-center">
-              <tbody>
-                <tr>
-                  <td>Username</td>
-                  <td><input type="text" value={this.state.txtUsername} onChange={(e) => { this.setState({ txtUsername: e.target.value }) }} /></td>
-                </tr>
-                <tr>
-                  <td>Password</td>
-                  <td><input type="password" value={this.state.txtPassword} onChange={(e) => { this.setState({ txtPassword: e.target.value }) }} /></td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td><input type="submit" value="LOGIN" onClick={(e) => this.btnLoginClick(e)} /></td>
-                </tr>
-              </tbody>
-            </table>
-          </form>
+        <div className="login-background">
+
+          <Helmet>
+            <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
+          </Helmet>
+
+
+          <div className="login-container">
+
+            <div className="login-content row">
+              <h2 className="col-12 text-login">ADMIN LOGIN</h2>
+              <form>
+                <table className="align-center">
+                  <tbody>
+                    <div className="col-12 form-group login-input">
+                      <tr>
+                        <td>Username</td>
+                        <td>
+                          <input
+                            type="text"
+                            value={this.state.txtUsername}
+                            className="form-control-1"
+                            onChange={(e) => { this.setState({ txtUsername: e.target.value }) }}
+                          />
+                        </td>
+                      </tr>
+                    </div>
+                    <div className="col-12 form-group login-input">
+                      <tr>
+                        <td>Password</td>
+                        <td className='custom-input'>
+                          <input
+                            type={this.state.isshowPassword ? 'text' : 'password'}
+                            value={this.state.txtPassword}
+                            className="form-control-2"
+                            onChange={(e) => { this.setState({ txtPassword: e.target.value }) }}
+
+                          />
+                          <span onClick={() => this.handleShowHidePassword()}>
+                            <i class={this.state.isshowPassword ? "fa-solid fa-eye" : "fa-solid fa-eye-slash"}>
+                            </i>
+                          </span>
+
+                        </td>
+                      </tr>
+                      <tr>
+                        <td></td>
+                        <div className="col-12" >
+                          <td>
+                            <input
+                              type="submit"
+                              value="LOGIN"
+                              className="form-login"
+                              onClick={(e) => this.btnLoginClick(e)}
+                            />
+                          </td>
+                        </div>
+                      </tr>
+                    </div>
+                  </tbody>
+                </table>
+              </form>
+            </div>
+          </div>
         </div>
       );
     }
     return (<div />);
   }
+
   // event-handlers
   btnLoginClick(e) {
     e.preventDefault();
@@ -51,6 +107,7 @@ class Login extends Component {
       alert('Please input username and password');
     }
   }
+
   // apis
   apiLogin(account) {
     axios.post('/api/admin/login', account).then((res) => {
@@ -64,4 +121,5 @@ class Login extends Component {
     });
   }
 }
+
 export default Login;
