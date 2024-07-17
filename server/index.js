82e0cb67f97730da1d522cb72912a4ca80245498
Huy <tuanhuyanime@gmail.com>
2024-07-17 17:44:23 +0700

    bai 1245346

--- server/index.js
+++ server/index.js
@@ -22,3 +22,15 @@ app.listen(PORT, () => {
 app.use('/api/customer', require('./api/customer.js'));
 
 app.use('/api/admin', require('./api/admin.js'));
+// deployment
+const path = require('path');
+// '/admin' serve the files at client-admin/build/* as static files
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
+});
+// '/' serve the files at client-customer/build/* as static files
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
