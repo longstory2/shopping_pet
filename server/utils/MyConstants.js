30a3f668cac8e30eb970f9fb9d418a8d8a2ce18b
longstory <nguyenphamquynhlong123@gmail.com>
2024-07-24 16:06:27 +0700

    The final

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,12 +1,12 @@
 const MyConstants = {
-  DB_SERVER: 'shopping.rzqoj6m.mongodb.net',
-  DB_USER: 'long',
+  DB_SERVER: '',
+  DB_USER: '',
   DB_PASS: '1234',
-  DB_DATABASE: 'shoppingonline',
+  DB_DATABASE: '',
   JWT_SECRET: '1234',
   JWT_EXPIRES: '99999999999999999',
-  EMAIL_USER: 'nguyenphamquynhlong123@gmail.com',
-  EMAIL_PASS: 'xywivtssfxutuzuq'
+  EMAIL_USER: 'abc@gmail.com',
+  EMAIL_PASS: ''
 };
 
 module.exports = MyConstants;
